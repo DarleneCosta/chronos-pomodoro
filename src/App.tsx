690ce4335778e824b1c0b82313c971d7590f80c2
Darlene Costa <darl_mel@hotmail.com>
2025-06-08 07:35:20 -0300

    component logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
   return (
    <Container>
-        <section>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam dolore nemo rem, vitae sequi provident excepturi laboriosam eius obcaecati cupiditate, non consequatur veritatis, eos molestiae ratione quidem quia commodi quis.
-        </section>
+        <Logo />
     </Container>
   );
 }
