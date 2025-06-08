ef8a0fc8f204ad2c915388e7fa0450c0c97f6590
Darlene Costa <darl_mel@hotmail.com>
2025-06-08 19:15:18 -0300

    component input

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { Input } from './components/Input';
 
 import './styles/global.css';
 import './styles/theme.css';
@@ -17,6 +18,29 @@ export function App() {
       </Container>
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <Input
+              type='text'
+              id='task'
+              placeholder='Digite a tarefa'
+              labelText='Tarefa'
+            />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.css';
+
+type InputProps = {
+  id: string;
+  labelText: string;
+} & React.ComponentProps<'input'>;
+
+export function Input(props: InputProps) {
+  return (
+    <>
+      <label htmlFor={props.id} className={styles.label}>
+        {props.labelText}
+      </label>
+      <input type={props.type} {...props} />
+    </>
+  );
+}
