eb6dd027d91cd23b15ede30f66cf9e3cc77dc0df
Darlene Costa <darl_mel@hotmail.com>
2025-06-11 06:16:29 -0300

    page home

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,11 @@
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { Input } from './components/Input';
-
 import './styles/global.css';
 import './styles/theme.css';
-import { Cycles } from './components/Cycles';
-import { Button } from './components/Button';
-import { PlayCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
+import { Home } from './pages/Home';
 
 export function App() {
   return (
     <>
-      <Container>
-        <Logo />
-      </Container>
-      <Container>
-        <Menu />
-      </Container>
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <Input
-              type='text'
-              id='task'
-              placeholder='Digite a tarefa'
-              labelText='Tarefa'
-            />
-          </div>
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className='formRow'>
-            <Cycles />
-          </div>
-          <div className='formRow'>
-            <Button>
-              <PlayCircleIcon />
-            </Button>
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
+      <Home />
     </>
   );
 }
