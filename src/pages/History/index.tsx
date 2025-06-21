bcc781de7cd59a479d3130763a50cbe7041292eb
Darlene Costa <darl_mel@hotmail.com>
2025-06-21 05:57:50 -0300

    adiciona tabela responsiva para exibição do histórico de pomodoros e botão para apagar histórico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,22 +1,62 @@
 import { useEffect } from 'react';
+import { TrashIcon } from 'lucide-react';
 import { Container } from '../../components/Container';
+import { Button } from '../../components/Button';
 import { Heading } from '../../components/Heading';
 import { MainTemplate } from '../../templates/MainTemplate';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import styles from './styles.module.css';
 
 export function History() {
+  const { state } = useTaskContext();
   useEffect(() => {
     document.title = 'Histórico de pomodoros | Chronos Pomodoro';
   }, []);
-
   return (
     <MainTemplate>
       <Container>
-        <Heading>Histórico</Heading>
+        <Heading>
+          <span>Histórico</span>
+          <span className={styles.buttonContainer}>
+            <Button
+              color='error'
+              aria-label='Apagar todo o histórico'
+              title='Apagar histórico'
+            >
+              <TrashIcon />
+            </Button>
+          </span>
+        </Heading>
       </Container>
+
       <Container>
-        <p style={{ textAlign: 'center' }}>
-          Aqui você pode ver o histórico de seus pomodoros.
-        </p>
+        <div className={styles.responsiveTable}>
+          <table>
+            <thead>
+              <tr>
+                <th>Tarefa</th>
+                <th>Duração</th>
+                <th>Data</th>
+                <th>Status</th>
+                <th>Tipo</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {state.tasks.map((task, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{task.name}</td>
+                    <td>{task.duration} min</td>
+                    <td>{new Date(task.startDate).toLocaleDateString() || ''}</td>
+                    <td>{task.completeDate ? 'Completa' : 'Incompleta'}</td>
+                    <td>{task.type}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </Container>
     </MainTemplate>
   );
